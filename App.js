151e20b6dd967ca15edbd7fffbb6b4c40bcd0272
Minh280 <150652470+Minh280@users.noreply.github.com>
2024-09-17 13:51:24 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World 1</h1>
+      <img src={logo} alt="Logo" /> {/* Add this line */}
+    </div>
+  );
+}
+
+export default App;
